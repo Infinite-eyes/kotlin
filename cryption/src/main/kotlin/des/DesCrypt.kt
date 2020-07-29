15c2a2c465356f90e2375118b2c5dd68e551c49e
Infinite-eyes <cwm930215@gmail.com>
2020-07-29 23:30:23 +0800

    1.add base64

--- cryption/src/main/kotlin/des/DesCrypt.kt
+++ cryption/src/main/kotlin/des/DesCrypt.kt
@@ -2,6 +2,7 @@ package des
 
 import com.sun.crypto.provider.DESCipher
 import java.security.Key
+import java.util.*
 import javax.crypto.Cipher
 import javax.crypto.SecretKey
 import javax.crypto.SecretKeyFactory
@@ -10,7 +11,7 @@ import javax.crypto.spec.DESKeySpec
 
 object DesCrypt {
 
-    fun encrypt(input: String, password: String): ByteArray {
+    fun encrypt(input: String, password: String): String {
 
 //    1.获取cipher 对象
         val cipher = Cipher.getInstance("DES")
@@ -22,13 +23,13 @@ object DesCrypt {
         cipher.init(Cipher.ENCRYPT_MODE, key)
 
         val encrypt = cipher.doFinal(input.toByteArray())
-
+        println("des加密密文：" + encrypt.size)
 //        println("des加密密文：$encrypt")
-        return encrypt;
+        return Base64.encode(encrypt)
     }
 
 
-    fun decrypt(input: ByteArray, password: String): ByteArray {
+    fun decrypt(input: String, password: String): ByteArray {
 
 //    1.获取cipher 对象
         val cipher = Cipher.getInstance("DES")
@@ -39,12 +40,11 @@ object DesCrypt {
         val key: Key? = kf.generateSecret(keySpec)
         cipher.init(Cipher.DECRYPT_MODE, key)
 
-        val decrypt = cipher.doFinal(input)
+        val decrypt = cipher.doFinal(Base64.decode(input))
 
 //        println("des解密密文：$decrypt")
         return decrypt;
     }
-
 
 
 }
@@ -53,9 +53,17 @@ fun main(args: Array<String>) {
 
     val input = "i love u"
     val password = "12345678"
-    val encrypt = DesCrypt.encrypt(input,password)
-    val decrypt = DesCrypt.decrypt(encrypt,password)
+
+    val array = input.toByteArray()
+    println("长度=" + array.size)
+    array.forEach {
+        println(it)
+    }
+
+
+    val encrypt = DesCrypt.encrypt(input, password)
+    val decrypt = DesCrypt.decrypt(encrypt, password)
     println("des加密密文：$encrypt")
-    println("des解密密文："+String(decrypt))
+    println("des解密密文：" + String(decrypt))
 
 }
