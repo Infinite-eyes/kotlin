14839d7d3192d077405c0008e605949eae757e6e
Infinite-eyes <cwm930215@gmail.com>
2020-08-05 01:18:45 +0800

    1.add cipher transform

--- cryption/src/main/kotlin/des/DesCrypt.kt
+++ cryption/src/main/kotlin/des/DesCrypt.kt
@@ -7,21 +7,32 @@ import javax.crypto.Cipher
 import javax.crypto.SecretKey
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.DESKeySpec
+import javax.crypto.spec.IvParameterSpec
 
 
 object DesCrypt {
 
+    //    val transformation = "DES/ECB/PKCS5Padding"
+    val transformation = "DES/CBC/PKCS5Padding"
+
+    val algorithm = "DES"
+
+
     fun encrypt(input: String, password: String): String {
 
 //    1.获取cipher 对象
-        val cipher = Cipher.getInstance("DES")
+        val cipher = Cipher.getInstance(transformation)
 
 //    2.初始化cipher
-        val kf = SecretKeyFactory.getInstance("DES")
+        val kf = SecretKeyFactory.getInstance(algorithm)
         val keySpec = DESKeySpec(password.toByteArray())
         val key: Key? = kf.generateSecret(keySpec)
-        cipher.init(Cipher.ENCRYPT_MODE, key)
 
+        val iv = IvParameterSpec(password.toByteArray())
+//        ECB
+//        cipher.init(Cipher.ENCRYPT_MODE, key)
+        //cbc
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv)
         val encrypt = cipher.doFinal(input.toByteArray())
         println("des加密密文：" + encrypt.size)
 //        println("des加密密文：$encrypt")
@@ -32,13 +43,19 @@ object DesCrypt {
     fun decrypt(input: String, password: String): ByteArray {
 
 //    1.获取cipher 对象
-        val cipher = Cipher.getInstance("DES")
+        val cipher = Cipher.getInstance(transformation)
 
 //    2.初始化cipher
-        val kf = SecretKeyFactory.getInstance("DES")
+        val kf = SecretKeyFactory.getInstance(algorithm)
         val keySpec = DESKeySpec(password.toByteArray())
         val key: Key? = kf.generateSecret(keySpec)
-        cipher.init(Cipher.DECRYPT_MODE, key)
+
+
+        val iv = IvParameterSpec(password.toByteArray())
+//        cipher.init(Cipher.DECRYPT_MODE, key)
+        //cbc
+        cipher.init(Cipher.DECRYPT_MODE, key, iv)
+//        cipher.init(Cipher.DECRYPT_MODE, key)
 
         val decrypt = cipher.doFinal(Base64.decode(input))
 
